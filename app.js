470f895a38c64dc29d7a387d8ef1899b496cf9e7
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-04-30 15:30:27 +0500

    delete element

--- app.js
+++ app.js
@@ -7,27 +7,24 @@ const submitBtn = document.querySelector('.submitBtn')
 // console.log(submitBtn)
 const listArea = document.querySelector('.listArea')
 // console.log(listArea)
-
-const itemArray = []
-let increment = 0;
-
+let itemArray = []
 
 function addItem() {
     if (inputValue.value !== "") {
         alertBox.textContent = "Item Added"
         alertBox.classList.remove('visibility')
         alertBox.classList.add('green')
-        const string = `<div class="listItems" id="${increment++}">
+        const string = `<div class="listItems">
         <ul>
-            <li>${inputValue.value}</li>
+        <li>${inputValue.value}</li>
         </ul>
         <div class="icons">
-            <i class="fa-regular fa-pen-to-square" style="color: #5c8ee6; cursor: pointer;"></i>
+        <i class="fa-regular fa-pen-to-square" style="color: #5c8ee6; cursor: pointer;"></i>
             <i class="fa-solid fa-trash" style="color: #ff3d3d; cursor: pointer;"></i>
-        </div>
-    </div>
-</div>`
-        itemArray.push(string)
+            </div>
+            </div>
+            </div>`
+            itemArray.push(string)
         inputValue.value = ""
     }
     else {
@@ -37,15 +34,10 @@ function addItem() {
     listArea.innerHTML = itemArray.join("")
     const deleteBtns = document.querySelectorAll('.fa-trash')
     // console.log(deleteBtn)
-    deleteBtns.forEach((deleteBtn)=>{
-        deleteBtn.addEventListener('click', (e)=>{
-            // e.target.parentNode.parentNode.remove()
-            const elementText = e.currentTarget.parentNode.parentNode
-            const index = itemArray.find((item)=> item === elementText)
-            console.log(elementText)
-            console.log(itemArray)
-            console.log(index)
-
+    deleteBtns.forEach((deleteBtn, index)=>{
+        deleteBtn.addEventListener('click', ()=>{
+            itemArray.splice(index, 1);
+            deleteBtn.parentElement.parentElement.remove()
         })
     })
 }
